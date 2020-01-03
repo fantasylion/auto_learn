cbcdf4f0df8e750fcf25655f27af0ff5ab57c668
serio.shi <serioshi@dingtalk.com>
2020-01-03 11:40:07 +0800

    amend code

--- learn.py
+++ learn.py
@@ -36,6 +36,7 @@ class Learn:
     learn_course = []
     current_course_index = 0
     current_training_index = 0
+    current_training_store_file = 'current_training.pkl'
 
     def __init__(self):
         chrome_options = Options()
@@ -50,13 +51,20 @@ class Learn:
         self.user = self.learn_config.get("account", "user_name")
         self.pwd = self.learn_config.get("account", "password")
         self.current_page = 1
-        self.learn_store = self.read_object()
+        self.learn_store = self.read_object(self.current_training_store_file)
 
     def open_main_page(self):
         allow_flash(self.browser, self.url)
         self.browser.maximize_window()
         self.browser.get(self.url)
         self.login_in()
+        self.start_course()
+
+    def start_course(self):
+        """
+        开始进入课程
+        :return:
+        """
         course = self.select_course()
         time.sleep(1)
         course.click()
@@ -64,10 +72,12 @@ class Learn:
             self.ready_to_read_video()
 
     def ready_to_read_video(self):
-        traning = self.select_traning()
-        time.sleep(1)
-        traning.click()
-        if self.into_start_study() is False:
+        """
+        进入课件详细页面 >> 进入视频页面 >> 点击视频播放按钮
+        :return:
+        """
+        self.into_trainning_info_page()
+        if self.into_video_page() is False:
             print('skip read video')
             self.return_to_training_list()
             return
@@ -77,6 +87,11 @@ class Learn:
         self.return_to_training_info()
         time.sleep(5)
         self.return_to_training_list()
+
+    def into_trainning_info_page(self):
+        traning = self.select_traning()
+        time.sleep(1)
+        traning.click()
 
     def return_to_training_info(self):
         """从视频播放页面返回到课件详情页"""
@@ -127,7 +142,7 @@ class Learn:
         for index, li in enumerate(lis):
             training = li.find_element_by_css_selector('.trainingPic')
             p_list = li.find_elements_by_css_selector('.training_infor p')
-            print(p_list[3].text)
+            print("第{}个课程状态：{}".format(str(index), p_list[3].text))
             if '培训状态： 未合格' == p_list[3].text:
                 self.current_course_index = index
                 return training
@@ -152,9 +167,21 @@ class Learn:
                 return study_link
         return None
 
-    def into_start_study(self):
+    def into_video_page(self):
         time.sleep(2)
         btns = self.browser.find_elements_by_css_selector('table tbody tr #button2')
+        bol = self.inspect_video_study_status(btns)
+        if bol is True:
+            btns[0].click()
+            return True
+        return False
+
+    def inspect_video_study_status(self, btns):
+        """
+        检查视频是否已经学习过了，如果学习过了存储到 pkl 文件中
+        :param btns:
+        :return:
+        """
         if btns[0].get_attribute('value') == '已学习':
             print('{}.{} this already readed.'.format(self.current_course_index, self.current_training_index))
             if self.current_course_index in self.learn_store:
@@ -162,18 +189,26 @@ class Learn:
             else:
                 self.learn_store.update({self.current_course_index: [self.current_training_index]})
             print(self.learn_store)
-            self.store_object(self.learn_store)
+            self.store_object(self.learn_store, self.current_training_store_file)
             return False
-        btns[0].click()
         return True
 
-    def store_object(self, current_training):
-        file_name = 'current_training.pkl'
+    def store_object(self, obj, file_name):
+        """
+        存储数据到文件
+        :param obj:
+        :param file_name:
+        :return:
+        """
         with open(file_name, 'wb') as file_store:
-            picklestring = pickle.dump(current_training, file_store)
+            pickle.dump(obj, file_store)
 
-    def read_object(self):
-        file_name = 'current_training.pkl'
+    def read_object(self, file_name):
+        """
+        读取文件数据
+        :param file_name:
+        :return:
+        """
         if os.path.exists(file_name):
             with open(file_name, 'rb') as file_store:
                 learn = pickle.load(file_store)
